a08886533863083a3f2b0fc23c58ede02a6d5328
dmsrashedul <wpansarali@gmail.com>
2025-04-12 16:42:45 +0600

    js pratice day 1

---
+++ main.js
@@ -0,0 +1,6 @@
+// Js Output
+document.getElementById("js").innerText = "Hello Word!";
+// document.write()
+document.write(5 + 6);
+window.alert("Click Ok To Next Step");
+console.log("Hellow World!");
